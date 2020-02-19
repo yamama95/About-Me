629d6bd7b46ad65c0d32e86d97b84eeb4aecf06c
yamama95 <yamamayahia954@gmail.com>
2020-02-20 00:29:41 +0200

    the latest

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 'use strict';
 var score =0;
-var userName  = function() { prompt('do you know me ? ').toLowerCase();
+var userName  = prompt('do you know me ? ').toLowerCase();
 
 if (userName === 'yes' || userName === 'y' )
 {
@@ -10,14 +10,14 @@ if (userName === 'yes' || userName === 'y' )
   alert('my name is yamama i am a software developer');
 } else {
   alert('please answer yes/y or no/n ');
-} };
-userName();
+}
+
 // console.log(userName);
 
 
 
 
-var hobby = function () {prompt('Do you know my hobby?').toLowerCase();
+var hobby = prompt('Do you know my hobby?').toLowerCase();
 
 if (hobby === 'yes' || hobby === 'y')
 {
@@ -28,10 +28,10 @@ if (hobby === 'yes' || hobby === 'y')
 }else {
   alert('please answer yes/y or no/n ');
 }
-}; hobby();
+
 //console.log( hobby);
 
-var webSite = function (){ prompt('do you know what is my website about?').toLowerCase();
+var webSite =  prompt('do you know what is my website about?').toLowerCase();
 if (webSite === 'yes'|| webSite === 'y' )
 {
   alert('Great!!');
@@ -40,12 +40,12 @@ if (webSite === 'yes'|| webSite === 'y' )
   alert('my website about video games');
 }else {
   alert('please answer yes/y or no/n ');
-} };
-webSite();
+}
+
 //console.log( webSite );
 
 
-var question = function () { prompt('are you intersted to my website?').toLowerCase();
+var question =  prompt('are you intersted to my website?').toLowerCase();
 if (question === 'yes' || question === 'y' )
 {
   alert('Great!!');
@@ -55,13 +55,13 @@ if (question === 'yes' || question === 'y' )
 }
 else {
   alert('please answer yes/y or no/n ');
-} };
-question();
+}
+
 //console.log( question );
 
 
 
-var inqury = function() { prompt('have you visited my website before?').toLowerCase();
+var inqury =  prompt('have you visited my website before?').toLowerCase();
 if (inqury === 'yes' || inqury === 'y')
 {
   alert('Great!!');
@@ -70,12 +70,12 @@ if (inqury === 'yes' || inqury === 'y')
   alert('you are welcome to my website');
 }else {
   alert('please answer yes/y or no/n ');
-} };
-inqury();
+}
+
 
 //console.log(inqury);
 
-var age = function(){
+
 for(var i=0 ; i <=3 ; i++){
   var age = prompt('guess what my age ?! (my age between 20 and 30)');
   age = Number(age);
@@ -92,22 +92,19 @@ for(var i=0 ; i <=3 ; i++){
   if (age >27 && age <30 ){ alert( 'too high');}
 }
 
-var favKind= ['action' , 'comedy' , 'horror' , 'adventure' ,'romance'].toLowerCase();
+var favKind= ['action' , 'comedy' , 'horror' , 'adventure' ,'romance'];
 for( var j =0 ; j <6 ; j++){
   var myFav = prompt ( ' Guess whats my favourite kind of films?');
-
-
+  var warn='noo!!try again :(';
   for( var x =0 ; x <favKind.length ; x++){
-
-  
-
     if ( myFav === favKind [x]) {
 
       j=6;
       x=favKind.length;
-      score++;
-      alert(' yup!!! you are right');
 
+      warn='yup you got it';
+      score++;
     }}
-  alert('nooo try again :(');}
+
+  alert(warn);}
 alert('you got ' + score +' lol');
