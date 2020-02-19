0fa3ac1df7417c895367f2828eaedc01c8906f50
yamama95 <yamamayahia954@gmail.com>
2020-02-19 22:48:05 +0200

    new-lab3

--- javascript/app.js
+++ javascript/app.js
@@ -73,42 +73,34 @@ if (inqury === 'yes' || inqury === 'y')
 
 
 for(var i=0 ; i <=3 ; i++){
-  var age = prompt('guess what my age ?!');
+  var age = prompt('guess what my age ?! (my age between 20 and 30)');
   age = Number(age);
-  if (age === 25 )
+  if (age === 24)
   {
     alert('yes thats right!Great!!');
     score++;
     break;
   }
-  if (age <=20){ alert('too low');}
-  if (age >=30 ){ alert( 'too high');}
-  if (age <29 && age >25 ){ alert( 'high');}
-  if (age >21 && age <25 ){ alert( 'law');}
+  if (age >20 && age <22){ alert('too low');}
+  if (age >=22 && age <24 ){ alert( 'law');}
+  if (age <=27 && age >24 ){ alert( 'high');}
+  if (age >27 && age <30 ){ alert( 'too high');}
 }
 
 var favKind= ['action' , 'comedy' , 'horror' , 'adventure' ,'romance'];
-var myFav = prompt ( ' Guess whats my favourite kind of films?');
-var attempts = 0;
-for( var j =0 ; j <=5 ; j++) {
-  for( var x =0 ; x <6 ; x++){
+for( var j =0 ; j <6 ; j++){
+  var myFav = prompt ( ' Guess whats my favourite kind of films?');
+
+
+  for( var x =0 ; x <favKind.length ; x++){
     if ( myFav === favKind [x]) {
+
+      j=6;
+      x=favKind.length;
+      score++;
       alert(' yup!!! you are right');
-      score=score+1;
 
-    } else {
-      attempts= attempts+1;
-      console.log( attempts,'score' ,score);
-    }
+    }}
+  alert('nooo try again :(');}
+alert('you got ' + score +' lol');
 
-  }
-  if ( attempts === 6) {
-    alert('try again');
-    myFav = prompt ( ' Guess whats my favourite kind of films?');
-    attempts = 0; }
-
-  else {
-    break;
-  }}
-document.write(score);
-document.write(favKind);
